ce666f6c6333c8e05b164d897eab9d3328966a40
Nathen Bowes <nathen.bowes@shopify.com>
2020-04-28 17:51:16 -0700

    code cleanup

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var express = require('express')
-var app = express()
-require('dotenv').config()
-
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const Multipassify = require('multipassify')
+require('dotenv').config()
+
+
 const multipassify = new Multipassify(process.env.MULTIPASS_SECRET)
 
 app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
@@ -17,14 +19,13 @@ app.post('/multipass', (req,res)=> {
   const { firstName,lastName, email } = req.body
   const multipassIdentifier = 'multipassNode'
 
-  var customerData = {
+  const customerData = {
     first_name: firstName,
     last_name: lastName,
     email,
     identifier: multipassIdentifier
   }
-  var token = multipassify.encode(customerData)
-  var url = multipassify.generateUrl(customerData, "bowes-guitars.myshopify.com")
+  const url = multipassify.generateUrl(customerData, "bowes-guitars.myshopify.com")
   return res.redirect(url)
 })
 
